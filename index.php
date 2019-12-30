63a2052fd57182dd3fd2f9f1bd8efde16fb75118
Paramastri Ardiningrum <paramastriii@gmail.com>
2019-12-30 19:30:57 +0700

    coba sidebar

--- index.php
+++ index.php
@@ -8,44 +8,38 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<link rel="icon" href="favicon.png" type="png" sizes="16x16">
 
-	<style>
-		@font-face {
-		font-family:'GothamRounded-Medium';
-		src:url('https://assets.lydian.io/fonts/GothamRounded-Medium.eot?#iefix') format('embedded-opentype'), url('https://assets.lydian.io/fonts/GothamRounded-Medium.otf') format('opentype'), url('https://assets.lydian.io/fonts/GothamRounded-Medium.woff') format('woff'), url('https://assets.lydian.io/fonts/GothamRounded-Medium.ttf') format('truetype'), url('https://assets.lydian.io/fonts/GothamRounded-Medium.svg#GothamRounded-Medium') format('svg');
-		font-weight:400;
-		font-style:normal
-		}
-		@font-face {
-		font-family:'GothamRounded-Book';
-		src:url('https://assets.lydian.io/fonts/GothamRounded-Book.eot?#iefix') format('embedded-opentype'), url('https://assets.lydian.io/fonts/GothamRounded-Book.otf') format('opentype'), url('https://assets.lydian.io/fonts/GothamRounded-Book.woff') format('woff'), url('https://assets.lydian.io/fonts/GothamRounded-Book.ttf') format('truetype'), url('https://assets.lydian.io/fonts/GothamRounded-Book.svg#GothamRounded-Book') format('svg');
-		font-weight:400;
-		font-style:normal
-		}
-		@font-face {
-		font-family:'GothamRounded-Light';
-		src:url('https://assets.lydian.io/fonts/GothamRounded-Light.eot?#iefix') format('embedded-opentype'), url('https://assets.lydian.io/fonts/GothamRounded-Light.otf') format('opentype'), url('https://assets.lydian.io/fonts/GothamRounded-Light.woff') format('woff'), url('https://assets.lydian.io/fonts/GothamRounded-Light.ttf') format('truetype'), url('https://assets.lydian.io/fonts/GothamRounded-Light.svg#GothamRounded-Light') format('svg');
-		font-weight:400;
-		font-style:normal
-		}
-		@font-face {
-		font-family:'Conv_GothamRnd-Bold';
-		src:url('https://assets.lydian.io/fonts/GothamRnd-Bold.eot') format('embedded-opentype'), url('https://assets.lydian.io/fonts/GothamRnd-Bold.woff') format('woff'), url('https://assets.lydian.io/fonts/GothamRnd-Bold.ttf') format('truetype'), url('https://assets.lydian.io/fonts/GothamRnd-Bold.svg') format('svg');
-		font-weight:400;
-		font-style:normal
-		}
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 
-	</style>
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style5.css">
+
+
+	 <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
 	<script>
 		 $(function(){
 
-    $(".dropdown-menu").on('click', 'a', function(){
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text());
-   });
+		    $(".dropdown-menu").on('click', 'a', function(){
+		      $(".btn:first-child").text($(this).text());
+		      $(".btn:first-child").val($(this).text());
+		   });
 
-});
+		});
+
+		 $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+                $(this).toggleClass('active');
+            });
+        });
   	</script>
 
 </head>
@@ -54,8 +48,80 @@
 
 <body>
 
+
+<div class="wrapper">
+        <!-- Sidebar Holder -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Menu</h3>
+            </div>
+
+            <ul class="list-unstyled">
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Home 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">Page 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+
+           
+        </nav>
+
+
+
+ <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="">
+
+                    <button style="margin-right: 30px;" type="button" id="sidebarCollapse" class="navbar-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                    <h2 style="font-family:'GothamRounded-Medium'; float: right;">Generate Nomor Surat</h2>
+                   <!--  <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button> -->
+
+
+                   
+                </div>
+            </nav>
+
+
+
 	<img style="height: 100px; margin-top: 30px;" src="logo.png" class="rounded mx-auto d-block">
-	<h2 style="margin-top: 50px; text-align: center; font-family:'GothamRounded-Medium';">Generate Nomor Surat Disini</h2>
 
 	<form style="margin: 0 auto; margin-top: 70px; width: 30%; font-family:'GothamRounded-Medium';">
 	  <div class="form-group">
