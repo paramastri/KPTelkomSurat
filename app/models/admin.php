9ded82eaffd236a4780b337f24bc022fc4228b79
putriendah11 <putriendah11@gmail.com>
2019-12-31 11:18:37 +0700

    membuat database

---
+++ app/models/admin.php
@@ -0,0 +1,8 @@
+<?php
+use Phalcon\Mvc\Model;
+class admin extends Model
+{
+    public $id;
+    public $username;
+    public $pass;
+}
---
+++ app/models/nomor_surat.php
@@ -0,0 +1,14 @@
+<?php
+use Phalcon\Mvc\Model;
+class nomor_surat extends Model
+{
+    public $id;
+    public $name;
+    public $nama_surat;
+    public $jenis_surat;
+    public $nomor;
+    public $no_surat;
+    public $tanggal;
+    public $nama_pengupload;
+    public $file;
+}
